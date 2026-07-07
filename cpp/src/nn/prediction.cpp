cbdf604198f1704c106100d90e73fc5372a6dd44
SShion0721 <113915813+SShion0721@users.noreply.github.com>
2026-07-08 01:38:17 +0800

    Use explicit pooling options for LibTorch portability

--- cpp/src/nn/prediction.cpp
+++ cpp/src/nn/prediction.cpp
@@ -3,7 +3,7 @@
 namespace tdmz {
 
 PredictionNetworkImpl::PredictionNetworkImpl(const NetworkConfig& cfg) : cfg_(cfg) {
-    pool = register_module("pool", torch::nn::AdaptiveAvgPool2d(torch::nn::AdaptiveAvgPool2dOptions({1, 1})));
+    pool = register_module("pool", torch::nn::AdaptiveAvgPool2d(torch::nn::AdaptiveAvgPool2dOptions(std::vector<int64_t>{1, 1})));
     fc = register_module("fc", torch::nn::Linear(cfg_.latent_channels, cfg_.hidden_channels));
     value = register_module("value", torch::nn::Linear(cfg_.hidden_channels, cfg_.value_dim));
     policy = register_module("policy", torch::nn::Linear(cfg_.hidden_channels, cfg_.action_space_size));
